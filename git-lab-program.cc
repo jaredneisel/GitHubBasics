45e5dd94e81ea9ed64204c0789de32032ca70751
Jared Neisel <jn820317@ohio.edu>
2019-01-23 19:17:21 -0500

    git-lab-program.cc File

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jared Neisel
+ *        Date: 01/23/2019
+ * Description: Git Lab Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+  cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
